a8e89d47cf25737c2a7e10e1f1742b08d9b00f23
Akramul Ratul <akramulratul@gmail.com>
2024-02-17 17:44:55 +0600

    fav icon change page title change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo
-        title="Meet Rose | Conference app "
+        title="Persona Therapy"
         titleTemplate="%s"
         defaultTitle=" Meet Rose | Conference app"
         description=" is an open source WebRTC project that gives you everything needed to build scalable and real-time audio and/or video experiences in your applications."
